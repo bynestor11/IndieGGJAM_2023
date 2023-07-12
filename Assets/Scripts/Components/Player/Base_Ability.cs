b167ff8b581135c7f720bd7adf17410e5ca8584b
bynestor11 <bynestor11@gmail.com>
2023-07-12 16:20:56 +0100

    Got LastDirectionLookedAt working

--- Assets/Scripts/Components/Player/Base_Ability.cs
+++ Assets/Scripts/Components/Player/Base_Ability.cs
@@ -6,5 +6,7 @@ using UnityEngine;
 
 public abstract class Base_Ability : MonoBehaviour
 {
+    public readonly VIKINGABILITIES ability_id;
+
     public abstract void Do_Ability();
 }
---
+++ Assets/Scripts/Components/Player/LastLookedAtDirection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DIRECTIONS {
+    Up,
+    Right,
+    Down,
+    Left
+}
+
+public class LastLookedAtDirection : MonoBehaviour
+{
+    [SerializeField]
+    public DIRECTIONS data;
+    
+    
+
+}
