82465e1b2a41dcce3290450e0da60afe9163c410
bynestor11 <bynestor11@gmail.com>
2023-07-12 15:23:17 +0100

    Added base for viking abilities

---
+++ Assets/Scripts/Components/Player/Ability_Array.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum VIKINGABILITIES {
+    BIG = 0,
+    AXE = 1
+}
+
+
+
+public class Ability_Array : MonoBehaviour
+{
+    public const int vikings_abilities_array_size = 2;
+    public bool[] viking_abilities = new bool[vikings_abilities_array_size];
+}
